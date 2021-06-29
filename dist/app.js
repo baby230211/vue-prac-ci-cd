0a31ab91326227f42546c442d532d89dad2d60cb
Sean <t4612310@gmail.com>
2021-06-29 16:32:07 +0800

    add express start server

---
+++ dist/app.js
@@ -0,0 +1,12 @@
+// public 的不會去編議
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.get(/js|css|img/, (req, res) => {
+  res.sendFile(`${__dirname}/${req.path}`);
+});
+app.get(/\//, (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+app.listen(port);
--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,8 +1,16 @@
-import { render } from '@testing-library/vue';
-import Counter from './Counter.vue';
+import { render, fireEvent } from "@testing-library/vue";
+import Counter from "./Counter.vue";
 
-test('Original title is 當前點擊次數0', () => {
+test("Original title is 當前點擊次數0", () => {
   const { getByTestId } = render(Counter);
-  const title = getByTestId('currentCounter');
-  expect(title.innerHTML).toBe('當前點擊次數0');
+  const title = getByTestId("currentCounter");
+  expect(title.innerHTML).toBe("當前點擊次數0");
+});
+
+test("After a click the title is 當前點擊次數1", async () => {
+  const { getByTestId, getByText } = render(Counter);
+  const incrementButton = getByText("點擊加1");
+  await fireEvent.click(incrementButton);
+  const title = getByTestId("currentCounter");
+  expect(title.innerHTML).toBe("當前點擊次數1");
 });
